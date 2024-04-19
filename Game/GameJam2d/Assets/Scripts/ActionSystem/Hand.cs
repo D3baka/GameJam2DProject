5ff41b52c9d62dc4f6bdc8e61cce002c6b431745
Daniel Stempfle <stempfledaniel@gmx.de>
2024-04-19 22:18:44 +0200

    Brrrr

--- Game/GameJam2d/Assets/Scripts/ActionSystem/Hand.cs
+++ Game/GameJam2d/Assets/Scripts/ActionSystem/Hand.cs
@@ -6,23 +6,22 @@ public class Hand : MonoBehaviour
 {
 
     [SerializeField] private HandCard[] cards;
-    private Monke[] monkes;
+    
+    [SerializeField] private Monke[] monkes;
 
     void Start()
     {
-        cards = new HandCard[4];
-        monkes = new Monke[4];
-
-        for (int i = 0; i < monkes.Length; i++)
-        {
-            monkes[i] = new Monke();
-        }
+      
     }
 
 
     public void playCard(int index)
     {
         Debug.Log(index);
+
+        Card.Type type = cards[index].getType();
+
+        cards[index].setType(monkes[index].drawCard());
     }
 
 }
--- Game/GameJam2d/Assets/Scripts/Monke.cs
+++ Game/GameJam2d/Assets/Scripts/Monke.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Monke : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<int> cards = new List<int>(4);
+    
+    
     void Start()
     {
-        
+        cards.Add((int)Card.Type.LEFT);
+        cards.Add((int)Card.Type.SHOOT);
+        cards.Add((int)Card.Type.LEFT);
+        cards.Add((int)Card.Type.RIGHT);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void addCard(Card.Type card)
     {
-        
+        cards.Add((int)card);
     }
 
-    
+    public void removeCard(Card.Type card)
+    {
+        cards.Remove((int)card);
+    }
+
+    public Card.Type drawCard()
+    {
+        // random card
+        int index = Random.Range(0, cards.Count);
+        Card.Type card = (Card.Type)cards[index];
+
+        return card;
+    }
 }
