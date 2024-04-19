461f2ccafe8e7e8814b95bf190a9557945ffc860
Daniel Stempfle <stempfledaniel@gmx.de>
2024-04-19 21:48:39 +0200

    Add card icon manager

---
+++ Game/GameJam2d/Assets/Scripts/ActionSystem/CardIconManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardIconManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] cardIcons;
+    public static CardIconManager Instance { get; private set; }
+    // Update is called once per frame
+
+    public Sprite GetCardIcon(Card.Type type)
+    {
+        if (cardIcons.Length < (int)type)
+        {
+            Debug.LogError("Card Icon not found for type: " + type);
+            return null;
+        }
+        
+        return cardIcons[(int)type];
+    }
+}
