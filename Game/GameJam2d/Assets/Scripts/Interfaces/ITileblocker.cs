2f230cb046fbd9414ba46575f778c7107fe8c06b
D3baka <derhesligebaum@gmail.com>
2024-04-20 09:41:10 +0200

    Added Gamemanager and Spawning/Moving functionality for Tileblockers in the Gridmanager

--- Game/GameJam2d/Assets/Scripts/Interfaces/ITileblocker.cs
+++ Game/GameJam2d/Assets/Scripts/Interfaces/ITileblocker.cs
@@ -1,4 +1,9 @@
+using UnityEngine;
+
 public interface ITileblocker
 {
-    
+    public Vector2 GetGridPosition();
+    public GameObject GetGameObject();
+
+    public void SetGridPosition(Vector2 position);
 }
