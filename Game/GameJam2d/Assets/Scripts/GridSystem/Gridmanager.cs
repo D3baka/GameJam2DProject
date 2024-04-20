b80d268996a915ad7c23411c18df552610d4794d
D3baka <derhesligebaum@gmail.com>
2024-04-20 11:45:11 +0200

    Added Playership and collisions with asteroids

--- Game/GameJam2d/Assets/Scripts/GridSystem/Gridmanager.cs
+++ Game/GameJam2d/Assets/Scripts/GridSystem/Gridmanager.cs
@@ -1,10 +1,12 @@
 using Debaka.Utils;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gridmanager : MonoBehaviour
 {
     [SerializeField] private Asteroid asteroid;
+    [SerializeField] private  PlayerShip playerShip;
     public static Gridmanager Instance { get; private set; }
 
     private Grid grid;
@@ -25,7 +27,9 @@ public class Gridmanager : MonoBehaviour
 
     private void Start()
     {
-        SpawnTileBlocker(SpaceGridTileBlocker.Asteroid, 0, grid.height - 1);   
+        SpawnTileBlocker(SpaceGridTileBlocker.PlayerShip, 5, 0);
+        SpawnTileBlocker(SpaceGridTileBlocker.Asteroid, 0, grid.height - 1);
+        SpawnTileBlocker(SpaceGridTileBlocker.Asteroid, 5, grid.height - 1);
     }
 
     public void NextTurn()
@@ -39,6 +43,15 @@ public class Gridmanager : MonoBehaviour
         {
             for (int y = 0; y < grid.height; y++)
             {
+                ITileblocker toMove;
+                if(grid.GetTileBlockerFromPosition(x,y, out toMove))
+                {
+                    if(toMove.GetGameObject().GetComponent<PlayerShip>() != null)
+                    {
+                        //Do nothing because we do not move the player in turns
+                        continue;
+                    }
+                }
                 ITileblocker oldBlocker;
                 MoveTileBlocker(x, y, x, y-1, out oldBlocker);
 
@@ -53,6 +66,9 @@ public class Gridmanager : MonoBehaviour
         {
             case SpaceGridTileBlocker.Asteroid:
                 tileblocker = SpawnAsteroid();
+                break;
+            case SpaceGridTileBlocker.PlayerShip:
+                tileblocker = SpawnPlayerShip();
                 break;
             default:
                 tileblocker = SpawnAsteroid();
@@ -69,14 +85,32 @@ public class Gridmanager : MonoBehaviour
         bool result = false;
         ITileblocker tileblocker;
         atPositionBefore = null;
+
+        // Check if we actually have something to move at the position
         if (grid.FreePosition(oldX, oldY, out tileblocker))
         {
+            //check if target position is in grid
             if (!IsInGrid(newX, newY))
             {
-                Destroy(tileblocker.GetGameObject());
+                DestroyTileBlocker(tileblocker);
                 return result;
             }
             result = grid.SetTileblockerAtPosition(newX, newY, tileblocker, out atPositionBefore);
+
+            //if there is a  Tileblocker at the target position      
+            if(result)
+            {
+                //if either the thing we move or the thing we collide with is the player ship
+                if (atPositionBefore.GetGameObject().GetComponent<PlayerShip>() != null)
+                {
+                    //interact!!!
+                    OnShipCollidedWithTileBlocker(tileblocker);
+                }
+                if(tileblocker.GetGameObject().GetComponent<PlayerShip>() != null)
+                {
+                    OnShipCollidedWithTileBlocker(atPositionBefore);
+                }
+            }
             tileblocker.SetGridPosition(new Vector2(newX, newY));
             tileblocker.GetGameObject().transform.position = grid.GetWorldPosition(newX, newY) + new Vector3(grid.cellSize / 2, grid.cellSize / 2, 0);
         }
@@ -89,14 +123,35 @@ public class Gridmanager : MonoBehaviour
         return newAsteroid;
     }
 
+    private ITileblocker SpawnPlayerShip()
+    {
+        PlayerShip newPlayerShip = Instantiate(playerShip);
+        return newPlayerShip;
+    }
+
     private bool IsInGrid(int x, int y)
     {
         return x >= 0 && y >= 0 && x < grid.width && y < grid.height;
     }
 
+    private void OnShipCollidedWithTileBlocker(ITileblocker collidingBlocker)
+    {
+        if(collidingBlocker.GetGameObject().GetComponent<Asteroid>() != null)
+        {
+            Debug.Log("Ship hit Asteroid!!!");
+            DestroyTileBlocker(collidingBlocker);
+        }        
+    }
+
+    private void DestroyTileBlocker(ITileblocker tileblocker)
+    {
+        Destroy(tileblocker.GetGameObject());
+    }
+
     public enum SpaceGridTileBlocker
     {
         Asteroid,
+        PlayerShip,
 
     }
 }
